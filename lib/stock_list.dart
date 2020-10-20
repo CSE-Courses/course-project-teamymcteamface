e5de152e9a178249a4092a9b47466d0bd4ed3662
messiahsb <42983908+messiahsb@users.noreply.github.com>
2020-10-20 14:08:06 -0400

    Added follow button

--- lib/stock_list.dart
+++ lib/stock_list.dart
@@ -29,14 +29,28 @@ class StockList extends StatelessWidget{
                       style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500)),
                   Text("${stock.company}",
                       style: TextStyle(color: Colors.black, fontSize: 20)),
+                        ButtonTheme(
+                    minWidth: 40.0,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      onPressed: () {
+                      },
+                      color: Colors.red,
+                      textColor: Colors.white,
+                      child: Text('Follow', style: TextStyle(fontSize: 10)),
+                    ),
+                  ),
+
 
                 ]),
             trailing: Column(children: <Widget>[
               Text("${stock.symbol}",
                   style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500)),
               Container(
-                width: 75,
-                height: 25,
+                width: 70,
+                height: 20,
                 child: Text("-1.09%", style: TextStyle(color: Colors.white)),
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
